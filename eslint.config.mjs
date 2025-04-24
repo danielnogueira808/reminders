3a05513a304ae8126050895b7e171a9fe413fedb
Daniel Nogueira <danielnogueira808@gmail.com>
2025-04-23 21:07:17 -0300

    style: increase printWidth to 120 and add max-len rule in eslint
    The printWidth in Prettier configuration has been increased from 80 to 120 to allow for longer lines of code, improving readability and reducing unnecessary line breaks. Additionally, the max-len rule has been added to the ESLint configuration to enforce this new line length limit consistently across the codebase.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,8 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      'linebreak-style': ['error', 'windows']
+      'linebreak-style': ['error', 'windows'],
+      'max-len': ['error', { code: 120 }],
     },
   },
 );
